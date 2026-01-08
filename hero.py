7a1040d18a3ebbc1f5e34abf52c8e8f58f8365c5
ethicalumang12 <umangraj255@gmail.com>
2026-01-08 23:19:30 +0530

    Update hero.py

--- hero.py
+++ hero.py
@@ -1111,7 +1111,7 @@ class HeroBot:
             return
 
         # 6. Send message to owner
-        triggers = ["umang se kaam h", "umang ko bulao", "umang ko bolna", "umang se kaam hai", "owner se bolo", "umang suno","umang"]
+        triggers = ["umang se kaam h", "umang ko bulao", "umang ko bolna", "umang se kaam hai", "owner se bolo", "umang suno","umang",]
         if any(word in text for word in triggers):
             owner_id_raw = os.getenv("OWNER_ID", "")
             owner_ids = [int(i.strip()) for i in owner_id_raw.split(",") if i.strip().isdigit()]
@@ -1249,6 +1249,7 @@ if __name__ == "__main__":
     if sys.platform.startswith("win"):
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     main()
+
 
 
 
